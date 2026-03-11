c8085413bcddc3ad5b953ea3ea50872d458dd2c3
samirbroukssi <buuz.web@gmail.com>
2026-03-11 20:52:04 +0100

    update

--- src/data/brands.ts
+++ src/data/brands.ts
@@ -2,26 +2,26 @@ export const brands = [
     {
         id: 1,
         name: "Dacia",
-        image: "/src/assets/images/brands/dacia.jpg"
+        image: "../../assets/images/brands/dacia.jpg"
     },
     {
         id: 2,
         name: "Hyundai",
-        image: "/src/assets/images/brands/hyundai.jpg"
+        image: "../../assets/images/brands/hyundai.jpg"
     },
     {
         id: 3,
         name: "Peugeot",
-        image: "/src/assets/images/brands/peugeot.jpg"
+        image: "../../assets/images/brands/peugeot.jpg"
     },
     {
         id: 4,
         name: "Renault",
-        image: "/src/assets/images/brands/renault.jpg"
+        image: "../../assets/images/brands/renault.jpg"
     },
     {
         id: 5,
         name: "Volkswagen",
-        image: "/src/assets/images/brands/volkswagen.jpg"
+        image: "../../assets/images/brands/volkswagen.jpg"
     }
 ];
